7e5d0283b3414ca353844b3df8c53cd99dcb3252
Shiquan Sun <ssqsxf@126.com>
2018-05-01 17:10:15 -0400

    Update scMF_test.R

--- scMF_test.R
+++ scMF_test.R
@@ -1,13 +1,11 @@
 
-num_data = 1
-num_pc = 5
+num_pc = 5 # number of PCs
 num_iter = 100
 X <- list(X1 = matrix(runif(20*100),ncol=20) );
 
 # initializations
 H0 <- list(H1 = matrix(runif(num_pc*20),ncol=20) );
 W0 <- list(W1 = matrix(runif(num_pc*100),ncol=num_pc) );
-U0 <- matrix(runif(num_pc*100,0,0),ncol=num_pc)
 criteria = 0; # set criteria=1 for KL divergence, criteria=0 for IS divergence
 rho = 1; # ADMM parameter
 library("Rcpp")
